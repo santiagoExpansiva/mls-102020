bf11275d2b751520cc7134d7dc224ece1ea08344
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-11-19 08:33:14 -0300

    agents v1

---
+++ l2/agentUpdateTemporaryEndpoints.test.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="agentUpdateTemporaryEndpoints" project="102020" enhancement="_blank" folder="" />
+
+ import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
+ export const integrations: ICANIntegration[] = [];
+ export const tests: ICANTest[] = [];
