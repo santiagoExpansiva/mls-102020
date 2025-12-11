cac39923e3515069160c3d57aef5862c6bb1fa55
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-11 17:42:46 -0300

    remove agents obsoletes

--- l2/agentScaffoldToOrganismMock.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="agentScaffoldToOrganismMock" project="102020" enhancement="_blank" folder="" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
