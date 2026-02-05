3da8b7af0e4b92bb125662c5bed57d7b4c71e352
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-05 09:17:23 -0300

    save: l2/enhancementAura.test.ts

--- l2/enhancementAura.test.ts
+++ l2/enhancementAura.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="enhancementAura" project="102020" enhancement="_blank" folder="" />
+/// <mls fileReference="_102020_/l2/enhancementAura.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
